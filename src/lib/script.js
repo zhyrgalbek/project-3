9e6239cbb3a365019eb19b006e406288a9f71977
zhyrgalbek <zhyrgalbek.kamalov@gmail.com>
2019-06-05 18:59:20 +0300

    add desktop ul.menu and form

--- src/lib/script.js
+++ src/lib/script.js
@@ -1,4 +1,18 @@
 $(document).ready(function(){
+	var w = $(window).width();
+	if(w > 600){
+		$(".hamburger").addClass("hamburger-drop");
+	} else {
+		$(".hamburger").removeClass("hamburger-drop");
+	}
+	$(window).resize(function(){
+		var w = $(window).width();
+		if(w > 600){
+			$(".hamburger").addClass("hamburger-drop");
+		} else {
+			$(".hamburger").removeClass("hamburger-drop");
+		}
+	});
 	$(".owl-carousel").owlCarousel({
 		// margin: 10,
 		loop: true,
@@ -26,28 +40,47 @@ $(document).ready(function(){
 		$(".hamburger").toggleClass("is-active");
 		$(".menu").toggleClass("menu-active");
 	});
-
+	
 	$(".form button").on("click",function(){
-		$(".nav-block").css("justify-content", "flex-end");
-		$(".form").css({
-			"width": "100%",
-			// "height": "50px"
+		var v = $(window).width();
+		$(".nav-block_right").css("justify-content", "flex-end");
+		$(".nav-block_right,form.form").css({
+			"width": "100%"
 		});
 		$(".search").css("display", "block");
-		$(".hamburger, .logo").css("display", "none");
+		if(v > 600){
+			$(".logo").css("display","block")
+			$("ul.menu").css("display", "none");
+			$(".nav-block_right,form.form").css({
+				"width": "500px"
+			});
+		} else {
+			$(".logo").css("display", "none");
+		}
+		$(".hamburger").addClass("hamburger-drop");
 		$(document).mouseup(function(e){
 			if(!$(e.target).is('[data-rel="drop"]')){
 				var w = $(window).width();
 				$(".nav-block").css("justify-content", "space-between");
-				$(".form").css("width", "40px");
+				$(".nav-block_right,form.form").css("width", "");
 				$(".search").css("display", "none");
 				if(w > 600){
-					$(".hamburger").css("display", "none");
+					$(".hamburger").addClass("hamburger-drop");
 					$(".logo").css("display", "block");	
-				} else if(w < 600) {
-					$(".hamburger, .logo").css("display", "block");
-				}	
+				} 
+				else if(w < 600) {
+					$(".hamburger").removeClass("hamburger-drop");
+					$(".logo").css("display", "block");
+				} 
+				if(v > 600){
+					$("ul.menu").css("display", "flex");
+				}
 			}
 		});
+		// if(w > 1055){
+		// 	$("form.form").css("display", "none");
+		// 	var li = $("li").addClass(".menu-li");
+		// 	$("ul.menu").aprepend(li);
+		// }
 	});
 });
